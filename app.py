084e4fbc71be4e7e9486f18bfcc5dea35d5c2ab5
Jeffri <i2211957@continental.edu.pe>
2024-03-27 03:10:55 +0000

    hello3

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h2>Jeffri Temis Cueva Fernandez</h2>'
+    return '<h2>Jeffri Temis Cueva Fernandez</h2>',
+           '<img src="https://shorturl.at/hlvFM"  width="500" height="600">'
 
 
 if __name__ == "__main__":
